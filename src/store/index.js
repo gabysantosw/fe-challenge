ba09079b802ee1c88258b767422d0e37bf33fba2
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-09-12 18:45:45 +0200

    Create action to fetch streams from database

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
-  actions: {},
+  actions: {
+    async fetchStreams() {
+      const request = await axios.get('api/streams.json');
+      return request.data;
+    }
+  },
   modules: {}
 });

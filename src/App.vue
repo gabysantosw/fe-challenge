0841e15e68746f2c5e03383c4223ec04ad9dde8e
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-09-12 18:46:57 +0200

    Add link to catalog

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav>
+      <router-link to="/">Catalog</router-link>
+    </nav>
     <router-view />
   </div>
 </template>

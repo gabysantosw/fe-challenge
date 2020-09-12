9b845e4224f7b1d87cdcc44eca1a4fd633193d48
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-09-12 22:01:08 +0200

    Add functionality to fetch games and sort the array of them

--- src/views/catalog.vue
+++ src/views/catalog.vue
@@ -1,20 +1,3 @@
-<template>
-  <div class="home">
-    <h1>Stream catalog</h1>
-    <h2>Now live</h2>
-    <ul>
-      <StreamCard v-for="(stream, index) of liveStreams" :stream="stream" :key="index" />
-    </ul>
-    <h2>Recommended</h2>
-    <ul>
-      <li v-for="(stream, index) of recommendedStreams" :key="index">
-        {{ stream.name }}
-      </li>
-    </ul>
-    <h2>Most followed games</h2>
-  </div>
-</template>
-
 <script>
 import { mapActions } from 'vuex';
 import StreamCard from '@/components/stream-card.vue';
@@ -24,14 +7,16 @@ export default {
   components: { StreamCard },
   async created() {
     this.streams = await this.fetchStreams();
+    this.games = await this.fetchGames();
   },
   data() {
     return {
-      streams: []
+      streams: [],
+      games: [],
     };
   },
   methods: {
-    ...mapActions(['fetchStreams'])
+    ...mapActions(['fetchStreams', 'fetchGames']),
   },
   computed: {
     liveStreams() {
@@ -43,7 +28,36 @@ export default {
     recommendedStreams() {
       // get the 3 best rated streams
       return [...this.streams].sort((current, next) => next.rating - current.ranting).splice(0, 3);
-    }
-  }
+    },
+    topFollowedGames() {
+      return [...this.games].sort((current, next) => next.followers - current.followers);
+    },
+  },
 };
 </script>
+
+<template>
+  <main>
+    <h1>Stream catalog</h1>
+    <h2>Now live</h2>
+    <ul class="card-list live">
+      <StreamCard v-for="(stream, index) of liveStreams" :stream="stream" :key="index" />
+    </ul>
+    <div class="side">
+      <h2>Top 3 streamers</h2>
+      <ol class="card-list">
+        <li class="card sorted" v-for="(stream, index) of recommendedStreams" :key="index">
+          {{ stream.name }}
+        </li>
+      </ol>
+    </div>
+    <div class="side">
+      <h2>Most followed games</h2>
+      <ul class="card-list">
+        <li class="card game" v-for="(game, index) of topFollowedGames" :key="index">
+          <span>{{ game.name }}</span> - {{ game.followers }} followers
+        </li>
+      </ul>
+    </div>
+  </main>
+</template>

b5daace4470f3fda61ea27ee31fa35161bea65ee
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2020-09-12 18:47:32 +0200

    Create card component to showcase streams

---
+++ src/components/stream-card.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'StreamCard',
+  props: {
+    stream: Object
+  }
+};
+</script>
+
+<template>
+  <li>
+    <img :src="stream.video" alt="" />
+    <br />
+    <router-link :to="{ name: 'StreamDetail', params: { stream: stream } }">{{ stream.name }}</router-link>
+    <p>Playing: {{ stream.game }}</p>
+  </li>
+</template>
